2e96a6fad8db5581cd4609555e2649d6f48fb808
ptran9102 <hoangphuctran9102@gmail.com>
2023-09-07 10:30:42 -0400

    Build Primary Draw

--- reactchat/src/pages/Home.tsx
+++ reactchat/src/pages/Home.tsx
@@ -1,11 +1,14 @@
-import { Box, CssBaseline } from "@mui/material";
+import CssBaseline from "@mui/material/CssBaseline";
+import Box from "@mui/material/Box";
 import PrimaryAppBar from "./templates/PrimaryAppBar";
+import PrimaryDraw from "./templates/PrimaryDraw";
 
 const Home = () => {
   return (
     <Box sx={{ display: "flex" }}>
       <CssBaseline />
       <PrimaryAppBar />
+      <PrimaryDraw></PrimaryDraw>
     </Box>
   );
 };
