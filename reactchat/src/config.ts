139e67b6dcd84a5fa5c10fbbcb8847a8d9bd2733
ptran9102 <hoangphuctran9102@gmail.com>
2023-12-22 22:58:59 -0500

    Deploy

--- reactchat/src/config.ts
+++ reactchat/src/config.ts
@@ -1,3 +1,3 @@
-export const BASE_URL = "mychats.up.railway.app/api";
-export const MEDIA_URL = "mychats.up.railway.app";
+export const BASE_URL = "http://mychats.up.railway.app/api";
+export const MEDIA_URL = "http://mychats.up.railway.app";
 export const WS_ROOT = "ws://mychats.up.railway.app";
